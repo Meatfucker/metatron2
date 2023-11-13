e126c5f63a5ff56d55752fc35a3981d679640927
Meatfucker <tramshed@gmail.com>
2023-11-12 20:56:45 -0500

    Lots of changes. LLM functionality mostly complete. Multimodal must wait for transformers to add Llava.

--- metatron2.py
+++ metatron2.py
@@ -7,11 +7,16 @@ import io
 import base64
 import math
 import re
+import sys
 from datetime import datetime
 import discord
 from discord import app_commands
-from wordgen import load_llm, wordgen, clearhistory
+from wordgen import load_llm, wordgen, clearhistory, Wordgenbuttons, deletelasthistory, inserthistory
 import asyncio
+from loguru import logger
+
+logger.remove()  # Remove the default configuration
+logger.add(sink=io.TextIOWrapper(sys.stdout.buffer, write_through=True), format="<light-black>{time:YYYY-MM-DD HH:mm:ss}</light-black> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<light-cyan>{function}</light-cyan> | <light-yellow>{message}</light-yellow>", level="INFO", colorize=True)
 
 SETTINGS = {}
 
@@ -33,29 +38,52 @@ class MetatronClient(discord.Client):
         self.wordgenmodel = None
         self.wordgentokenizer = None
         self.wordgen_queue = asyncio.Queue()
+        self.wordgenview_lastmessage = {}
 
-    async def setup_hook(self): 
+    async def setup_hook(self):
+        logger.info("Logging in...")
         await self.tree.sync()
         if SETTINGS["enableword"][0] == "True":
             self.wordgenmodel, self.wordgentokenizer = await load_llm()
-        self.loop.create_task(client.process_wordgen_queue())
+        self.loop.create_task(client.process_queue())
 
     async def on_ready(self):
-        print("Logged in")
-        
-    async def process_wordgen_queue(self):
+        logger.info("Login Successful")
+    
+    async def process_queue(self):
         while True:
-            (message, stripped_message, kwargs) = await self.wordgen_queue.get()
-
-            if kwargs.get('forget', True):
+            args = await self.wordgen_queue.get()
+            action = args[0]
+            if action == 'wordgenforget':
+                message = args[1]
                 await clearhistory(message)
-            else:
-                response = await wordgen(message, stripped_message, self.wordgenmodel, self.wordgentokenizer, SETTINGS["wordsystemprompt"][0], SETTINGS["wordnegprompt"][0])
+                logger.success(f'WORDGEN History cleared.')
+            elif action == 'wordgengenerate':
+                message = args[1]
+                stripped_message = args[2]
+                response = await wordgen(args[1], args[2], self.wordgenmodel, self.wordgentokenizer, SETTINGS["wordsystemprompt"][0], SETTINGS["wordnegprompt"][0])
+                if message.author.id in self.wordgenview_lastmessage:
+                    try:
+                        await self.wordgenview_lastmessage[message.author.id].delete()
+                    except discord.NotFound:
+                        pass  # Message not found, might have been deleted already
                 chunks = [response[i:i+1500] for i in range(0, len(response), 1500)]
                 for chunk in chunks:
                     await message.channel.send(chunk)
-            self.wordgen_queue.task_done()
-
+                new_message = await message.channel.send(view=Wordgenbuttons(self.wordgen_queue, message.author.id, message, stripped_message))
+                self.wordgenview_lastmessage[message.author.id] = new_message
+                logger.success(f'WORDGEN Reply:{stripped_message}')
+            elif action == 'wordgendeletelast':
+                await deletelasthistory(message)
+                logger.success(f'WORDGEN Last History Pair Delete')
+            elif action == 'wordgenimpersonate':
+                userid = args[1]
+                prompt = args[2]
+                llmprompt = args[3]
+                await inserthistory(userid, prompt, llmprompt, SETTINGS["wordsystemprompt"][0])
+            else:
+                logger.error(f'QUEUE Error, Unknown function:{action}:{args}')
+            
     async def on_message(self, message):
         if self.user.mentioned_in(message):
             if SETTINGS["enableword"][0] != "True":
@@ -63,9 +91,20 @@ class MetatronClient(discord.Client):
                 return #check if LLM generation is enabled
             stripped_message = re.sub(r'<[^>]+>', '', message.content).lstrip()
             if "forget" in stripped_message.lower():
-                await self.wordgen_queue.put((message, stripped_message, {'forget': True}))
+                await self.wordgen_queue.put(('wordgenforget', message))
             else:
-                await self.wordgen_queue.put((message, stripped_message, {'forget': False}))
+                await self.wordgen_queue.put(('wordgengenerate', message, stripped_message))
+        await self.process_commands(message)
    
 client = MetatronClient(intents=discord.Intents.all()) #client intents
-client.run(SETTINGS["token"][0]) #run bot
+
+@client.tree.command()
+async def impersonate(interaction: discord.Interaction, userprompt: str, llmprompt: str):
+    """Slash command that allows for one shot prompting"""
+    await client.wordgen_queue.put(('wordgenimpersonate', interaction.user.id, userprompt, llmprompt))
+    await interaction.response.send_message(f'History inserted:\n User: {userprompt}\n LLM: {llmprompt}')
+    
+client.run(SETTINGS["token"][0], log_handler=None) #run bot
+
+
+   
--- wordgen.py
+++ wordgen.py
@@ -5,56 +5,130 @@ import os
 import sys
 import time
 import torch
+import io
+import json
 import asyncio
+from loguru import logger
+import discord
 
 wordgen_user_history = {}
 
 async def load_llm():
-
-    model = LlamaForCausalLM.from_pretrained("liuhaotian/llava-v1.5-13b", load_in_4bit=True, bnb_4bit_compute_dtype=torch.float16, bnb_4bit_use_double_quant=True, low_cpu_mem_usage=True, device_map="auto")
+    '''loads the llm'''
+    model = LlamaForCausalLM.from_pretrained("liuhaotian/llava-v1.5-13b", load_in_4bit=True, bnb_4bit_compute_dtype=torch.float16, bnb_4bit_use_double_quant=True, low_cpu_mem_usage=True, device_map="auto") #load model
+    model = model.to_bettertransformer() #Use bettertransformers for more speed
     model.eval()
-    tokenizer = LlamaTokenizer.from_pretrained("liuhaotian/llava-v1.5-13b")
+    logger.success("Model Loaded.")
+    tokenizer = LlamaTokenizer.from_pretrained("liuhaotian/llava-v1.5-13b") #load tokenizer
+    logger.success("Tokenizer Loaded.")
     return model, tokenizer
     
 async def wordgen(message, prompt, model, tokenizer, systemprompt="You are an AI", negativeprompt=""):
-    
-    userhistory = await loadhistory(message)
-    print(f'USER HISTORY {userhistory}')
+    '''function for generating responses with the llm'''
+    userhistory = await loadhistory(message) #load the users past history to include in the prompt
     if message.author.id not in wordgen_user_history or not wordgen_user_history[message.author.id]:
-        formattedprompt = f'{systemprompt}\n\n USER: {prompt}\nASSISTANT:'
+        formattedprompt = f'{systemprompt}\n\nUSER:{prompt}\nASSISTANT:' #if there is no history, add the system prompt to the beginning
     else:
-        formattedprompt = f'{userhistory}\n USER: {prompt}\nASSISTANT:'
-    input_ids = tokenizer.encode(formattedprompt, return_tensors="pt")
-    input_ids = input_ids.to('cuda')
-    negative_input_ids = tokenizer.encode(negativeprompt, return_tensors="pt")
-    negative_input_ids = negative_input_ids.to('cuda')
-    output = await asyncio.to_thread(model.generate, input_ids, max_length=4096, temperature=0.2, do_sample=True, guidance_scale=2, negative_prompt_ids=negative_input_ids)
-    generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
-    response_index = generated_text.rfind("ASSISTANT:")
+        formattedprompt = f'{userhistory}\nUSER:{prompt}\nASSISTANT:'
+    input_ids = tokenizer.encode(formattedprompt, return_tensors="pt") #turn prompt into tokens
+    input_ids = input_ids.to('cuda') #send tokens to gpu
+    negative_input_ids = tokenizer.encode(negativeprompt, return_tensors="pt") #turn negative prompt into tokens
+    negative_input_ids = negative_input_ids.to('cuda') #negative tokens to gpu
+    logger.info("WORDGEN Generate Started.")
+    with torch.backends.cuda.sdp_kernel(enable_flash=True, enable_math=False, enable_mem_efficient=False): #enable flash attention for faster inference
+        output = await asyncio.to_thread(model.generate, input_ids, max_length=4096, temperature=0.2, do_sample=True, guidance_scale=2, negative_prompt_ids=negative_input_ids) #run the inference in a thread so it doesnt block the bots execution
+    generated_text = tokenizer.decode(output[0], skip_special_tokens=True) #turn the returned tokens into string
+    logger.success("WORDGEN Generate Completed")
+    response_index = generated_text.rfind("ASSISTANT:") #this and the next line extract the bots response for posting to the channel
     llmresponse = generated_text[response_index + len("ASSISTANT:"):].strip()
-    await savehistory(generated_text, message, systemprompt)
+    await savehistory(generated_text, message, systemprompt) #save the response to the users history
     return(llmresponse)
     
 async def savehistory(generated_text, message, systemprompt):
-
-    last_message_index = generated_text.rfind("USER:")
+    '''saves the prompt and llm response to the users history'''
+    last_message_index = generated_text.rfind("USER:") #this and the next line extract the last question/answer pair from the generated text
     last_message_pair = generated_text[last_message_index:].strip()
-    if message.author.id not in wordgen_user_history:
+    if message.author.id not in wordgen_user_history: #if they have no history yet include the system prompt along with the special tokens for the instruction format
         wordgen_user_history[message.author.id] = []
         messagepair = f'{systemprompt}\n\n{last_message_pair}</s>\n'
     else:
         messagepair = f'{last_message_pair}</s>\n'
-    wordgen_user_history[message.author.id].append(messagepair)
+    wordgen_user_history[message.author.id].append(messagepair) #add the message to the history
     
 async def loadhistory(message):
     
     if message.author.id in wordgen_user_history and wordgen_user_history[message.author.id]:
-        # Combine all strings in the user's history list into one string
         combined_history = ''.join(wordgen_user_history[message.author.id])
         return combined_history
-    else:
-        return None
-
+    
 async def clearhistory(message):
+    
     if message.author.id in wordgen_user_history:
         del wordgen_user_history[message.author.id]
+
+async def deletelasthistory(message):
+    
+    if message.author.id in wordgen_user_history:
+        wordgen_user_history[message.author.id].pop()
+
+async def inserthistory(userid, prompt, llmprompt, systemprompt):
+
+    if userid not in wordgen_user_history:
+        wordgen_user_history[userid] = []
+        injectedhistory = f'{systemprompt}\n\nUSER:{prompt}\nASSISTANT:{llmprompt}</s>\n'
+    else:
+        injectedhistory = f'USER:{prompt}\nASSISTANT:{llmprompt}</s>\n'
+    wordgen_user_history[userid].append(injectedhistory)
+        
+class Wordgenbuttons(discord.ui.View):
+    """Class for the ui buttons on speakgen"""
+
+    def __init__(self, wordgen_queue, userid, message, strippedmessage):
+        super().__init__()
+        self.timeout = None
+        self.wordgen_queue = wordgen_queue
+        self.userid = userid
+        self.message = message
+        self.strippedmessage = strippedmessage
+
+    @discord.ui.button(label='Reroll last reply', emoji="🎲", style=discord.ButtonStyle.grey)
+    @logger.catch   
+    async def reroll(self, interaction: discord.Interaction, button: discord.ui.Button):
+        """Rerolls last reply"""
+        if self.userid == interaction.user.id:
+            await interaction.response.send_message("Rerolling...", ephemeral=True, delete_after=5)
+            await self.wordgen_queue.put(('wordgendeletelast', self.message, self.strippedmessage))
+            await self.wordgen_queue.put(('wordgengenerate', self.message, self.strippedmessage))
+            
+
+    @discord.ui.button(label='Delete last reply', emoji="❌", style=discord.ButtonStyle.grey)
+    @logger.catch
+    async def delete_message(self, interaction: discord.Interaction, button: discord.ui.Button):
+        """Deletes message"""
+        if self.userid == interaction.user.id:
+            await self.wordgen_queue.put(('wordgendeletelast', self.message, self.strippedmessage))
+            await interaction.response.send_message("Last question/answer pair deleted", ephemeral=True, delete_after=5)
+            
+         
+    @discord.ui.button(label='Show History', emoji="📜", style=discord.ButtonStyle.grey)
+    @logger.catch   
+    async def dm_history(self, interaction: discord.Interaction, button: discord.ui.Button):
+        """Prints history to user"""
+        if self.userid == interaction.user.id:
+            if self.message.author.id in wordgen_user_history:
+                historyfile = io.BytesIO(json.dumps(wordgen_user_history[self.userid], indent=1).encode())
+                await interaction.response.send_message(ephemeral=True, file=discord.File(historyfile, filename='history.txt'))
+            else:
+                await interaction.response.send_message("No History", ephemeral=True, delete_after=5)
+            logger.info("WORDGEN Show History.")
+         
+    @discord.ui.button(label='Wipe History', emoji="🤯", style=discord.ButtonStyle.grey)
+    @logger.catch   
+    async def delete_history(self, interaction: discord.Interaction, button: discord.ui.Button):
+        """Deletes history"""
+        if self.userid == interaction.user.id:
+            await self.wordgen_queue.put(('wordgenforget', self.message, self.strippedmessage))
+            await interaction.response.send_message("History wiped", ephemeral=True, delete_after=5)
+
+
+
