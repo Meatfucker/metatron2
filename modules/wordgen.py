e1cfbf434a5e1e7767d1600f02cc6e4e6985178f
Meatfucker <tramshed@gmail.com>
2024-01-05 06:03:32 -0500

    Fix solar generate call not being threaded and locking up client until finished.

--- modules/wordgen.py
+++ modules/wordgen.py
@@ -131,9 +131,7 @@ class WordQueueObject:
                     settings.top_p = 0.37
                     settings.top_k = 100
                     settings.token_repetition_penalty = 1.18
-                    #settings.disallow_tokens(self.metatron.llm_tokenizer, [self.metatron.llm_tokenizer.eos_token_id])
-                    #generator.warmup()
-                    result = generator.generate_simple(formatted_prompt, settings, 2000)
+                    result = await asyncio.to_thread(generator.generate_simple,formatted_prompt, settings, 2000)
 
                     llm_generate_logger.debug("WORDGEN Generate Completed")
 
